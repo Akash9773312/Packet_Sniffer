37e3522f52767389c542cbecd1a4962cb7bf9374
Akash9773312 <am9773312@outlook.com>
2023-03-24 23:06:36 +0530

    output2 commit

--- packet_sniffer/output.py
+++ packet_sniffer/output.py
@@ -7,6 +7,7 @@ from abc import ABC, abstractmethod
 
 
 class Output(ABC):
+
     def __init__(self, subject):
         subject.register(self)
 
@@ -31,22 +32,22 @@ class OutputToScreen(Output):
               "data. Press Ctrl-C to abort...\n")
 
     def update(self, frame) -> None:
-        self_frame = frame 
+        self._frame = frame
         self._display_output_header()
-        self.display_protocol_info()
+        self._display_protocol_info()
         self._display_packet_contents()
-    
+
     def _display_output_header(self) -> None:
         local_time = time.strftime("%H:%M:%S", time.localtime())
         print(f"[>] Frame #{self._frame.packet_num} at {local_time}:")
 
-    def display_protocol_info(self) -> None:
+    def _display_protocol_info(self) -> None:
         for proto in self._frame.protocol_queue:
             try:
                 getattr(self, f"_display_{proto.lower()}_data")()
             except AttributeError:
                 print(f"{'':>4}[+] Unknown Protocol")
-            
+
     def _display_ethernet_data(self) -> None:
         ethernet = self._frame.ethernet
         interface = "all" if self._frame.interface is None \
@@ -60,40 +61,41 @@ class OutputToScreen(Output):
 
     def _display_ipv4_data(self) -> None:
         ipv4 = self._frame.ipv4
-        print(f"{i}[+] IPv4{ipv4.src:.>27} -> {ipv4.dst: <15}")
-        print(f"{2 * i} DSCP:{ipv4.dscp}")
-        print(f"{2 * i} Total length:{ipv4.len}")
-        print(f"{2 * i} ID:{ipv4.id}")
-        print(f"{2 * i} Flags:{ipv4.flags_str}")
-        print(f"{2 * i} TTL:{ipv4.ttl}")
-        print(f"{2 * i} Protocol:{ipv4.encapsulated_proto}")
-        print(f"{2 * i} Header Checksum:{ipv4.chksum_hex_str}")
+        print(f"{i}[+] IPv4 {ipv4.src:.>27} -> {ipv4.dst: <15}")
+        print(f"{2 * i}  DSCP: {ipv4.dscp}")
+        print(f"{2 * i}  Total Length: {ipv4.len}")
+        print(f"{2 * i}  ID: {ipv4.id}")
+        print(f"{2 * i}  Flags: {ipv4.flags_str}")
+        print(f"{2 * i}  TTL: {ipv4.ttl}")
+        print(f"{2 * i}  Protocol: {ipv4.encapsulated_proto}")
+        print(f"{2 * i}  Header Checksum: {ipv4.chksum_hex_str}")
 
     def _display_ipv6_data(self) -> None:
         ipv6 = self._frame.ipv6
-        print(f"{i}[+] IPv6{ipv6.src:.>27} ->{ipv6.dst:<15}")
-        print(f"{2 * i} Traffic Class:{ipv6.tclass_hex_str}")
-        print(f"{2 * i} Flow Label:{ipv6.flabel_txt_str}")
-        print(f"{2 * i} Payload Length:{ipv6.payload_len}")
-        print(f"{2 * i} Next Header:{ipv6.encapsulated_proto}")
-        print(f"{2 * i} Hop Limit:{ipv6.hop_limit}")
+        print(f"{i}[+] IPv6 {ipv6.src:.>27} -> {ipv6.dst: <15}")
+        print(f"{2 * i}  Traffic Class: {ipv6.tclass_hex_str}")
+        print(f"{2 * i}  Flow Label: {ipv6.flabel_txt_str}")
+        print(f"{2 * i}  Payload Length: {ipv6.payload_len}")
+        print(f"{2 * i}  Next Header: {ipv6.encapsulated_proto}")
+        print(f"{2 * i}  Hop Limit: {ipv6.hop_limit}")
 
     def _display_arp_data(self) -> None:
         arp = self._frame.arp
-        if arp.open == 1:
-            print(f"{i}[+] ARP who has {arp.tpa:.>18} ? -> Tell{arp.spa}")
-        else:
-            print(f"{i}[+] ARP {arp.tpa:.>28} -> Is at {arp.sha}")
-        print(f"{2 * i} Hardware Type:{arp.htype}")
-        print(f"{2 * i} Protocol Type:{arp.ptype_str}"f"({arp.ptype_hex_str})")
-        print(f"{2 * i} Hardware Length:{arp.hlen}")
-        print(f"{2 * i} Protocol Length:{arp.plen}")
-        print(f"{2 * i} Operation:{arp.oper} ({arp.oper_str})")
-        print(f"{2 * i} Sender Hardware Address:{arp.sha}")
-        print(f"{2 * i} Sender Protocol Address:{arp.spa}")
-        print(f"{2 * i} Target Hardware Address:{arp.tha}")
-        print(f"{2 * i} Target Protocol Address:{arp.tpa}")
-    
+        if arp.oper == 1:  # ARP Request
+            print(f"{i}[+] ARP Who has {arp.tpa:.>18} ? -> Tell {arp.spa}")
+        else:              # ARP Reply
+            print(f"{i}[+] ARP {arp.spa:.>28} -> Is at {arp.sha}")
+        print(f"{2 * i}  Hardware Type: {arp.htype}")
+        print(f"{2 * i}  Protocol Type: {arp.ptype_str} "
+              f"({arp.ptype_hex_str})")
+        print(f"{2 * i}  Hardware Length: {arp.hlen}")
+        print(f"{2 * i}  Protocol Length: {arp.plen}")
+        print(f"{2 * i}  Operation: {arp.oper} ({arp.oper_str})")
+        print(f"{2 * i}  Sender Hardware Address: {arp.sha}")
+        print(f"{2 * i}  Sender Protocol Address: {arp.spa}")
+        print(f"{2 * i}  Target Hardware Address: {arp.tha}")
+        print(f"{2 * i}  Target Protocol Address: {arp.tpa}")
+
     def _display_tcp_data(self) -> None:
         tcp = self._frame.tcp
         print(f"{i}[+] TCP {tcp.sport:.>28} -> {tcp.dport: <15}")
